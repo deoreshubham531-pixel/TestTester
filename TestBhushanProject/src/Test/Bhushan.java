0520cbde301ecd36c70a74a0590ba48d0365c6d8
Shubham Vijay Deore <deoreshubham531@gmail.com>
2026-06-14 19:47:38 +0530

    these is the initial commit

---
+++ TestBhushanProject/src/Test/Bhushan.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class Bhushan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my name is bhushan i am the software tester");
+		
+		int a=2;
+		int b= 9;
+		int sum;
+		
+		sum=a+b;
+	    System.out.println("the sum of the two numbers : "+ sum);
+		 
+		 
+
+	}
+
+}
